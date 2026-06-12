e77a52523ca372e770c4d87946c7221441c9888e
Giovanni Rovesta <giovannirovesta@gmail.com>
2026-06-12 11:40:57 -0300

    Create 10-adivinhacao.cpp

---
+++ 2-bimestre/10-adivinhacao.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib> 
+#include <ctime> 
+using namespace std;
+
+int main() {
+    srand(time(0)); 
+    int numero = rand() % 100 + 1;
+    int palpite, tentativas = 0;
+
+    cout << "Vamos jogar um jogo de adivinhacao? Escolhemos um numero e voce tera que adivinha-lo!" << endl;
+    cout << "Adivinhe o numero entre 1 e 100:" << endl;
+
+    while (true) {
+        cin >> palpite;
+        tentativas++;
+
+        if (palpite == numero) {
+            cout << "Acertou! Total de tentaivas: " << tentativas << endl;
+            break;
+        }
+        else if (palpite > numero) {
+            cout << "Muito alto!" << endl;
+        }
+        else {
+            cout << "Muito baixo!" << endl;
+        }
+    }
+
+    return 0;
+}
