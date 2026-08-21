291a9ee8b0acba0903be0c162c5768541983ce9f
Giovanni Rovesta <giovannirovesta@gmail.com>
2026-08-21 09:08:27 -0300

    Create ex-4.py

---
+++ 3-bimestre/Phyton/ex-4.py
@@ -0,0 +1,10 @@
+contador = 1
+soma = 0
+
+n = int(input('Digite um número positivo: \n'))
+if n > 0:
+    while contador <= n:
+        if contador % 2 == 0:
+            soma = contador + soma
+        contador += 1
+print(f'A soma dos pares é: {soma}')
